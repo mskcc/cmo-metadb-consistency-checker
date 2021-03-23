40f81bbfda55f38433e7fcd5f77ebf6e2506db1c
Divya Madala <71040191+divyamadala30@users.noreply.github.com>
2021-03-23 10:53:33 -0400

    Tests now test for null and empry values
    If a value is null or empty, the field is removed before the
    consistency check happens
    
    Signed-off-by: Divya Madala <71040191+divyamadala30@users.noreply.github.com>

--- src/main/java/org/mskcc/cmo/metadb/util/ConsistencyCheckerUtil.java
+++ src/main/java/org/mskcc/cmo/metadb/util/ConsistencyCheckerUtil.java
@@ -1,21 +1,25 @@
 package org.mskcc.cmo.metadb.util;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.google.common.base.Strings;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 import org.mskcc.cmo.common.FileUtil;
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.skyscreamer.jsonassert.JSONCompareMode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ArrayNode;
-import com.fasterxml.jackson.databind.node.ObjectNode;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.LinkedHashMap;
-import java.util.Map;
 
 @Component
 public class ConsistencyCheckerUtil {
@@ -27,15 +31,31 @@ public class ConsistencyCheckerUtil {
     private String metadbCheckerFailuresFilepath;
 
     private final ObjectMapper mapper = new ObjectMapper();
-    private final String[] DEFAULT_IGNORED_FIELDS = new String[]{"metaDbRequestId", "metaDbSampleId", "metaDbPatientId", "requestJson", "samples"};
+    private final String[] DEFAULT_IGNORED_FIELDS = new String[]{
+        "metaDbRequestId",
+        "metaDbSampleId",
+        "metaDbPatientId",
+        "requestJson",
+        "samples"};
 
-    private final String CONSISTENCY_CHECKER_FAILURES_FILE_HEADER = "DATE\tREASON\tREFERENCEJSON\tTARGETJSON\n";
+    private final String CONSISTENCY_CHECKER_FAILURES_FILE_HEADER =
+            "DATE\tREASON\tREFERENCEJSON\tTARGETJSON\n";
     private final String MISSING_OR_NULL_JSON = "JSON string is NULL";
     private final String SUCCESSFUL_CONSISTENCY_CHECK = "Both JSON strings are equal";
     private final String FAILED_CONSISTENCY_CHECK = "JSON strings are NOT equal";
 
-    public boolean isConsistent(String referenceJson, String targetJson, String[] ignoredFields) throws Exception {
-        //File loggerFile = fileUtil.getOrCreateFileWithHeader(metadbCheckerFailuresFilepath, CONSISTENCY_CHECKER_FAILURES_FILE_HEADER);
+    /**
+     *
+     * @param referenceJson
+     * @param targetJson
+     * @param ignoredFields
+     * @return boolean
+     * @throws Exception
+     */
+    public boolean isConsistent(String referenceJson, String targetJson,
+            String[] ignoredFields) throws Exception {
+        //File loggerFile = fileUtil.getOrCreateFileWithHeader(metadbCheckerFailuresFilepath,
+        //CONSISTENCY_CHECKER_FAILURES_FILE_HEADER);
         if (referenceJson == null ? targetJson == null : referenceJson.equals(targetJson)) {
             return true;
         }
@@ -49,13 +69,18 @@ public class ConsistencyCheckerUtil {
                     targetFilteredJsonMap.get("requestNode"), JSONCompareMode.STRICT);
             JSONAssert.assertEquals(referenceFilteredJsonMap.get("samplesNode"),
                     targetFilteredJsonMap.get("samplesNode"), JSONCompareMode.STRICT);
+
             assertResponse = true;
             //fileUtil.writeToFile(loggerFile, SUCCESSFUL_CONSISTENCY_CHECK);
-        } catch(Exception e) {
+        } catch (Exception e) {
             //fileUtil.writeToFile(loggerFile, FAILED_CONSISTENCY_CHECK);
             e.printStackTrace();
         }
         return assertResponse;
+    }
+
+    public boolean isConsistent(String referenceJson, String targetJson) throws Exception {
+        return isConsistent(referenceJson, targetJson, DEFAULT_IGNORED_FIELDS);
     }
 
     /**
@@ -65,7 +90,8 @@ public class ConsistencyCheckerUtil {
      * @return ArrayNode
      * @throws JsonProcessingException
      */
-    private ArrayNode getOrderedRequestSamplesJson(ArrayNode samples, String[] ignoredFields) throws JsonProcessingException {
+    private ArrayNode getOrderedRequestSamplesJson(ArrayNode samples,
+            String[] ignoredFields) throws JsonProcessingException {
         Map<String, JsonNode> unorderedSamplesMap = new HashMap<>();
         Iterator<JsonNode> itr = samples.elements();
         while (itr.hasNext()) {
@@ -88,10 +114,13 @@ public class ConsistencyCheckerUtil {
         return sortedJsonNodeSamples;
     }
 
-    public boolean isConsistent(String referenceJson, String targetJson) throws Exception {
-        return isConsistent(referenceJson, targetJson, DEFAULT_IGNORED_FIELDS);
-    }
-
+    /**
+     *
+     * @param reason
+     * @param referenceJson
+     * @param targetJson
+     * @return string
+     */
     public String failureMessageAppender(String reason, String referenceJson, String targetJson) {
         String currentDate = LocalDate.now().format(DateTimeFormatter.ISO_LOCAL_DATE);
         StringBuilder builder = new StringBuilder();
@@ -112,11 +141,13 @@ public class ConsistencyCheckerUtil {
      * @return Map
      * @throws JsonProcessingException
      */
-    private Map<String, String> getRequestJsonNodesMap(String jsonString, String[] ignoredFields) throws JsonProcessingException {
+    private Map<String, String> getRequestJsonNodesMap(String jsonString,
+            String[] ignoredFields) throws JsonProcessingException {
         JsonNode unfilteredJsonNode = mapper.readTree(jsonString);
         ArrayNode samplesJsonNode = null;
         if (unfilteredJsonNode.has("samples")) {
-            samplesJsonNode = getOrderedRequestSamplesJson((ArrayNode) unfilteredJsonNode.get("samples"), ignoredFields);
+            samplesJsonNode = getOrderedRequestSamplesJson(
+                    (ArrayNode) unfilteredJsonNode.get("samples"), ignoredFields);
         }
 
         JsonNode filteredNode = filterJsonNode((ObjectNode) unfilteredJsonNode, ignoredFields);
@@ -137,11 +168,19 @@ public class ConsistencyCheckerUtil {
                 node.remove(field);
             }
         }
-        // check value of node, if empty or null then remove also
-        // will need to iterate through the node itself after
-        // going through and removing the ignored fields above
-        // see example iterator set up in getOrderedRequestSamplesJson()
+
+        Iterator<String> itr = node.fieldNames();
+        List<String> removeFieldNames = new ArrayList<>();
+        while (itr.hasNext()) {
+            String fieldName = itr.next();
+            JsonNode value = node.get(fieldName);
+            if (Strings.isNullOrEmpty(value.asText()) || value.asText().equals("null")) {
+                removeFieldNames.add(fieldName);
+            }
+        }
+        for (String field : removeFieldNames) {
+            node.remove(field);
+        }
         return node;
     }
-
 }
